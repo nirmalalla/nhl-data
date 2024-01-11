d4dbcb58170d72adec2815f007b3948ef6f682c4
nirmalalla <allanirmal@gmail.com>
2024-01-11 14:49:09 -0600

    Adding multioutput regressor for art ross

--- server/src/data_analysis/art_ross_trophy.py
+++ server/src/data_analysis/art_ross_trophy.py
@@ -3,40 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from sklearn.multioutput import MultiOutputRegressor
 
 winners = {
-    2023: "Connor McDavid",
-    2022: "Connor McDavid",
-    2021: "Connor McDavid",
-    2020: "Leon Draisaitl",
-    2019: "Nikita Kucherov",
-    2018: "Connor McDavid",
-    2017: "Connor McDavid",
+    2015: "Jamie Benn",
     2016: "Patrick Kane",
-    2015: "Jamie Benn"
+    2017: "Connor McDavid",
+    2018: "Connor McDavid",
+    2019: "Nikita Kucherov",
+    2020: "Leon Draisaitl",
+    2021: "Connor McDavid",
+    2022: "Connor McDavid",
+    2023: "Connor McDavid"
 }
 
-def calculateValue(points, icetime, games):
-    return (points / (games))
+def calculateValue(goals, points, icetime, games):
+    return [goals / games, points / games, icetime / games]
 
 def createDataframe():
-    values = {}
+    #Creating a 2D array to store previous winners with their data
+    values = []
     for key in winners.keys():
-        values[key] = gatherData(winners.get(key), key)
+        values.append(gatherData(winners.get(key), key))
 
-    df = pd.DataFrame(columns=winners.keys())
-    df.loc[len(df)] = values
-    return df
+    return values
+
 
 def gatherData(name, year):
     tmp_df = pd.read_csv("../data/_data - skaters" + str(year) + ".csv")
     tmp_df = tmp_df.loc[tmp_df["name"] == name]
 
+    goals = tmp_df.at[tmp_df.index[0], "I_F_goals"]
     points = tmp_df.at[tmp_df.index[0], "I_F_points"]
     icetime = tmp_df.at[tmp_df.index[0], "icetime"]
     games = tmp_df.at[tmp_df.index[0], "games_played"]
 
-    return calculateValue(points, icetime, games)
+    return calculateValue(goals, points, icetime, games)
 
 def gather2023Data():
     df = pd.read_csv("../data/_data - skaters2023.csv")
@@ -51,26 +53,40 @@ def gather2023Data():
         tmp_df = df.loc[df["name"] == name]
         games = tmp_df.at[tmp_df.index[0], "games_played"]
         icetime = tmp_df.at[tmp_df.index[0], "icetime"]
-        position = tmp_df.at[tmp_df.index[0], "position"]
+        goals = tmp_df.at[tmp_df.index[0], "I_F_goals"]
         points = tmp_df.at[tmp_df.index[0], "I_F_points"]
         
-        if (games >= threshold_games and icetime >= threshold_icetime and position != "D"):
+        if (games >= threshold_games and icetime >= threshold_icetime):
             X.append([2023])
-            value = calculateValue(points, icetime, games)
+            value = calculateValue(goals, points, icetime, games)
             y.append(value)
             name_values.append([name, value])
 
     return name_values
 
 
+def findDistance(data, pred):
+    #Calculating total distance from predicted value
+    total = 0
+    index = 0
+
+    for n in data:
+        total += abs(pred[index] - n)
+        index += 1
+
+    return total
+
 def findClosest(name_values, pred):
+    #Looping through the name_values to find the top 5 closest players
     min = []
     for n in name_values:
-        if (len(min) < 5 or abs(n[1] - pred) < abs(min[len(min) - 1][1] - pred)):
+        distance = findDistance(n[1], pred)
+
+        if (len(min) < 5 or distance < min[len(min) - 1][1]):
             if(len(min) == 5):
-                min[4] = [n[0], n[1]]
+                min[4] = [n[0], distance]
             else:
-                min.append([n[0], n[1]])
+                min.append([n[0], distance])
             
             min = sorted(min, key=lambda x: x[1])
 
@@ -78,24 +94,25 @@ def findClosest(name_values, pred):
 
 
 def artRossTrophyData():
-    df = createDataframe()
-
     X_train = []
 
     for key in winners.keys():
         X_train.append([key])
 
     
-    y_train = np.array(df.values[0])
+    y_train = createDataframe()
     
-    regr =  LinearRegression()
+    #Fitting the model with a regressive model using the data
+    regr =  MultiOutputRegressor(LinearRegression())
     regr.fit(X_train, y_train)
 
     X_test = np.array([[2022.5], [2023], [2023.5]])
 
+    #Testing the data with new values
     y_pred = regr.predict(X_test)
-    
+
+    #Finding the top 5 closest and returning it
     name_values = gather2023Data()
-    closest_player = findClosest(name_values, y_pred[0])
+    closest_player = findClosest(name_values, y_pred[1])
 
     return closest_player
